6f64f3dc5eaeee91b8d9289b067ecbed8071a1e6
Eduardo Lima <eduardolima.tecnico@gmail.com>
2022-11-05 00:35:43 -0300

    feat: mongoose connection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const router = express.Router();
+
+// Conecta ao banco
+mongoose.connect("mongodb://eduardo:eduardo@localhost:27017/admin");
 
 // Carregar rotas
 const indexRoute = require('./routes/index-route');
